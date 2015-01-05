dba8be3ea4cbe0bc998714cd06ee2cafe9b90626
Jens Wille <jens.wille@gmail.com>
2015-01-05 14:51:17 +0100

    lib/solr4r/result.rb (initialize): Don't restrict to supported types (e.g. system info).

--- lib/solr4r/result.rb
+++ lib/solr4r/result.rb
@@ -5,7 +5,7 @@
 #                                                                             #
 # solr4r -- A Ruby client for Apache Solr                                     #
 #                                                                             #
-# Copyright (C) 2014 Jens Wille                                               #
+# Copyright (C) 2014-2015 Jens Wille                                          #
 #                                                                             #
 # Mir is free software: you can redistribute it and/or modify it under the    #
 # terms of the GNU Affero General Public License as published by the Free     #
@@ -37,19 +37,14 @@ module Solr4R
     extend Forwardable
     def_delegators :to_hash, :fetch, :deep_fetch, :%
 
-    def self.type_for(hash)
-      constants.find { |const|
+    def initialize(response, hash)
+      self.class.constants.find { |const|
         if hash.key?(const.to_s.downcase)
-          mod = const_get(const)
-          return mod if mod.is_a?(Module)
+          mod = self.class.const_get(const)
+          break extend(mod) if mod.is_a?(Module)
         end
       }
 
-      raise TypeError, "could not find supported type: #{hash.keys.join(', ')}"
-    end
-
-    def initialize(response, hash)
-      extend(self.class.type_for(hash))
       @response, @hash = response, hash.extend(Nuggets::Hash::DeepFetchMixin)
     end
 
